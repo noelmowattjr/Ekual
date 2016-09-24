1071ea29f7803063503762c4bb6e2b39f6e22232
Noel Mowatt <nwmowatt@fullsail.edu>
2016-09-24 11:27:40 -0400

    Connected UI Buttons to Code
    Connected UI Buttons to Code and resized logo on launch screen

--- Ekual/Ekual/ViewController.swift
+++ Ekual/Ekual/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //--Display Label
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //---------------BUTTON ACTIONS---------------//
+    
+    //--Numbers
+    @IBAction func NumberClicked(_ sender: UIButton) {
+        
+    }
+    
+    //--Operators
+    @IBAction func OperatorClicked(_ sender: UIButton){
+        
+    }
+    
+    //--Equals
+    @IBAction func EqualsClicked(_ sender: UIButton) {
+        
+    }
+    
+    //--Clear
+    @IBAction func ClearClicked(_ sender: UIButton) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
