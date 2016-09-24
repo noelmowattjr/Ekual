9ed9e2e41c1c6f016433b4b587b728aaea9fb287
Noel Mowatt <nwmowatt@fullsail.edu>
2016-09-24 19:47:26 -0400

    Testing buttons
    Tested buttons to confirm that they are in fact connect to my code!

--- Ekual/Ekual/ViewController.swift
+++ Ekual/Ekual/ViewController.swift
@@ -23,21 +23,66 @@ class ViewController: UIViewController {
     //--Numbers
     @IBAction func NumberClicked(_ sender: UIButton) {
         
+        switch sender.tag {
+        case 0:
+            print("0 button pressed")
+        case 1:
+            print("1 button pressed")
+        case 2:
+            print("2 button pressed")
+        case 3:
+            print("3 button pressed")
+        case 4:
+            print("4 button pressed")
+        case 5:
+            print("5 button pressed")
+        case 6:
+            print("6 button pressed")
+        case 7:
+            print("7 button pressed")
+        case 8:
+            print("8 button pressed")
+        case 9:
+            print("9 butt pressed")
+        default:
+            return
+        }
     }
     
     //--Operators
     @IBAction func OperatorClicked(_ sender: UIButton){
-        
+        switch sender.tag {
+        case 10:
+            print("+ button pressed")
+        case 11:
+            print("- button pressed")
+        case 12:
+            print("x button pressed")
+        case 13:
+            print("/ button pressed")
+        default:
+            return
+        }
     }
     
     //--Equals
     @IBAction func EqualsClicked(_ sender: UIButton) {
-        
+        switch sender.tag {
+        case 14:
+            print("= button pressed")
+        default:
+            return
+        }
     }
     
     //--Clear
     @IBAction func ClearClicked(_ sender: UIButton) {
-        
+        switch sender.tag {
+        case 15:
+            print("clear button pressed")
+        default:
+            return
+        }
     }
     
     override func didReceiveMemoryWarning() {
